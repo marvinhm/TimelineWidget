23b6e1ec5a5fd1947f2ce05ad860441e018d6e3d
marvinhm <marvinmatovu@gmail.com>
2023-10-21 12:39:59 +0100

    add swiperjs with upper & lower sections + styles

--- app/pages/TimelineContainer.tsx
+++ app/pages/TimelineContainer.tsx
@@ -1,5 +1,16 @@
 "use client";
-import { useState } from 'react'
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// import required modules
+import { Keyboard, Navigation } from 'swiper/modules';
 
 const steps: TimelineStep[] = [
   {
@@ -10,12 +21,12 @@ const steps: TimelineStep[] = [
   {
     id: 2,
     title: "Step 2",
-    description: "This is step one!"
+    description: "This is step two!"
   },
   {
     id: 3,
     title: "Step 3",
-    description: "This is step one!"
+    description: "This is step three!"
   }
 ]
 
@@ -26,38 +37,62 @@ interface TimelineStep {
 }
 
 function TimelineContainer() {
-  const [timelineData, setTimelineData] = useState<Array<TimelineStep> | null>(steps);
+  const [timelineData, setTimelineData] = useState<Array<TimelineStep>>(steps);
 
-  function handleChange(event: React.SyntheticEvent) {
-
-  }
 
   return (
-    <form className='TimelineContainer'>
-      {
-        timelineData?.map((item) => (
-          <div className='timelineBox' key={item.id}>
-            <div className='tl-left'>
-              <textarea 
-                  defaultValue={item.title} 
-                  placeholder="type something descriptive"
-                  onChange={handleChange}
-                  name={item.title}
-              />
-            </div>
-            <div className='tl-right'>
-              <textarea 
-                  defaultValue={item.description} 
-                  placeholder="type something descriptive"
-                  onChange={handleChange}
-                  name={item.description}
-              />
-            </div>
-          </div>
-        ))
-      }
-    </form>
-  )
+    <div className="timelineContainer">
+      <div className="row row1--timeline_section">
+        <div className="col-md-12">
+          <Swiper
+            slidesPerView={2}
+            spaceBetween={0}
+            keyboard={{
+              enabled: true,
+            }}
+            navigation={true}
+            modules={[Keyboard, Navigation]}
+            className="swiper-size"
+          >
+            {timelineData?.map((item) => (
+              <SwiperSlide key={item.id} className="swiper-wrapper-container">
+                <div className="single--timeline_section tl-top-row">
+                  
+                  <div className="content--timeline_section flex inline-flex">
+                    <div className="step--timeline_section">
+                      <div className="step-title--timeline_section m-0 font-bold">
+                        <textarea 
+                          rows={1} 
+                          cols={item.title.length}
+                          placeholder="Step Title"
+                          name="firstName"
+                          value={item.title}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="single--timeline_section relative tl-bottom-row">
+                  <div className="content--timeline_section flex inline-flex">
+                    <div className="step-desc--timeline_section">
+                      <textarea
+                        rows={1} 
+                        cols={item.description.length}
+                        placeholder="Step Description"
+                        name="firstName"
+                        value={item.description}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TimelineContainer;
